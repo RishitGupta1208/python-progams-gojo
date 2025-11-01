e7bc64624305acf12a84bccfcf9d9641afecbce0
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-01 11:34:35 +0530

    commit

---
+++ activity21.py
@@ -0,0 +1,14 @@
+x=float(input("please enter your height in cm:"))
+y=float(input("please enter your weight in kg:"))
+z=y/(x/100)**2
+print("your BMI is",z)
+if z<=16.2:
+    print("you are severly underweight")
+elif z<=18.4:
+    print("you are underweight")
+elif z<=24.9:
+    print("you are healthy")
+elif z<=19.9:
+    print("you are overweight")
+else:
+    print("you are obese")
