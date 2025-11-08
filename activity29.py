62ea8f11f27f3b70ee2e2c2954a65c398e770a66
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-08 11:19:06 +0530

    commit

---
+++ activity29.py
@@ -0,0 +1,7 @@
+print("welcome to divisible checker")
+a=int(input("please enter a number:"))
+b=int(input("please enter number2:"))
+if a%b==0:
+    print(a,"is divisible by",b)
+else:
+    print("the numbers are not divisible")
