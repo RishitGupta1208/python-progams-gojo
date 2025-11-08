63124989cfcb16e735a0c478109831630e8fd370
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-08 11:37:01 +0530

    akaza

---
+++ activity31.py
@@ -0,0 +1,10 @@
+B1=10
+B2=20
+B3=30
+average=B1+B2+B3/3
+if B1<average:
+    print(" Biker 1 is slowrer than average speed")
+elif B2<average:
+    print(" Biker 2 is slowrer than average speed")
+else:
+    print(" Biker 3 is slowrer than average speed")
