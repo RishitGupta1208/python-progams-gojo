1534541730c965155380d87c9ea22ee0a2e66090
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-16 11:38:38 +0530

    geto

---
+++ lesson 1 activity 1/activity38..py
@@ -0,0 +1,6 @@
+string=str(input("please enter your string:"))
+string2=('')
+for i in string:
+    string2=i+string2
+print("\n the oringnal string=",string)
+print("\n the reverse string=",string2)
