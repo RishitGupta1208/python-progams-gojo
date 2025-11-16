6da0b2a01d4478bc3164557a900dca55f75240d9
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-16 11:25:23 +0530

    tori

---
+++ activtity34.py
@@ -0,0 +1,36 @@
+print("select one 1.car 2.bike")
+pc=int(input("select an option:"))
+if pc==1:
+    print("which model will you prefer 1.sedan 2.Suv")
+    choice1=int(input("enter your choice:"))
+    if choice1==1:
+        print("which model 1. bugati 2. lambo")
+        choice2=int(input("enter your choice:"))
+        if choice2==1:
+            print("thank you for your time")
+            if choice2==2:
+                print("thanks for you time")
+            if choice1==2:
+               print("which model 1. austin martin 2. jeep")
+               choice2=int(input("enter your choice:"))
+            if choice2==2:
+               print("thank you for your time")
+            
+else:
+    if pc==2:
+       print("which model will you prefer 1.bikes 2.scooty")
+    choice1=int(input("enter your choice:"))
+    if choice1==1:
+        print("which model 1.kavasaki  2. KTM")
+        choice2=int(input("enter your choice:"))
+        if choice2==1:
+            print("thank you for your time")
+            if choice2==2:
+                print("thank you for your time")
+            if choice1==2:
+               print("which model 1. austin martin 2. jeep")
+               choice2=int(input("enter your choice:"))
+            if choice2==2:
+               print("thank you for your time")
+    
+        
---
+++ lesson 1 activity 1/activity37.py
@@ -0,0 +1,5 @@
+num=int(input("please enter how many times you want to repeat:"))
+sum=0
+for i in range(1,num+1):
+    sum=sum+i
+print(sum)
