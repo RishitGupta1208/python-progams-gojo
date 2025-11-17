319d5162bb08445f5ed4d04f37c69d0a6d4744da
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-17 16:10:42 +0530

    goku

---
+++ lesson 1 activity 1/activity36.py
@@ -0,0 +1,6 @@
+poi=int(input(" please enter your age: "))
+if poi>20:
+    print(" you are not between 10 to 20 years ")
+if poi>=10:
+    if poi<=20:
+        print(" you are between 10 and 20 years")
