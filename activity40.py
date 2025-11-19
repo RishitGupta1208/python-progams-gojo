fe45d1163a753177fa3193c4946348d50c5a8eb9
rishitgupta1208 <rishitgupta1208@gmail.com>
2025-11-19 15:56:36 +0530

    nezuko

---
+++ activity40.py
@@ -0,0 +1,6 @@
+num1=int(input(" please enter the amount ti take the power of: "))
+num2=int(input(" please enter the power of the number you want to calculate: "))
+result=1
+for i in range(num2):
+    result=result*num1
+print(" result = ",result)
