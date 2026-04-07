25cd35212613607699ceefc724525ba8e70263a4
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-04-07 17:15:50 +0530

    bb

--- activity64.py
+++ activity64.py
@@ -4,5 +4,8 @@ class circle:
     def area(self):
         ef=3.14*self.radius**2
         print(ef)
+    def perimeter(self):
+        er=6.28*self.radius
 call=circle(9)
 call.area()
+call.perimeter()
