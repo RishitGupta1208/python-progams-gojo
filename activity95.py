b77fedca2c8e0ca5f71e2ea54f98f1f5fae2e2ae
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-06-01 21:14:22 +0530

    Tkinter

--- activity95.py
+++ activity95.py
@@ -7,13 +7,13 @@ lbl = Label(text="Hey There!", fg="white", bg="#072F5F", height=1, width=300)
 name_lbl = Label(text="Full Name", bg="#3895D3")
 name_entry = Entry()
 def display():
-    name=name_entry.get()
-    global message 
-    message="Welcome to the application\ntoday's date is:"
-    greet = "Hello "+name+"\n"
-    text_box.insert(END, greet)
-    text_box.insert(END, message)
-    text_box.insert(END, date.today())
+	name = name_entry.get()
+	global message
+	message = "Welcome to the Application! \nToday's date is: "
+	greet = "Hello "+name+"\n"
+	text_box.insert(END, greet)
+	text_box.insert(END, message)
+	text_box.insert(END, date.today())
 text_box = Text(height=3)
 btn = Button(text="Begin", command=display, height=1, bg="#1261A0", fg='white')
 lbl.pack()
@@ -21,4 +21,4 @@ name_lbl.pack()
 name_entry.pack()
 btn.pack()
 text_box.pack()
-root.mainloop
+root.mainloop()
