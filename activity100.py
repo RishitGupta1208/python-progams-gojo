89411cc88466bdb18b14dd054339d18d624051dc
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-06-03 12:26:14 +0530

    color

---
+++ activity100.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Two Sprites with Custom Color Change Event")
+CHANGE_COLOR = pygame.USEREVENT + 1
+pygame.time.set_timer(CHANGE_COLOR, 2000)
+class ColorSprite(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, color):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.color = color
+        self.image.fill(self.color)
+        self.rect = self.image.get_rect(topleft=(x, y))
+    def change_color(self):
+        self.color = (random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
+        self.image.fill(self.color)
+sprite1 = ColorSprite(200, 250, 100, 100, (255, 0, 0))
+sprite2 = ColorSprite(500, 250, 100, 100, (0, 0, 255))
+all_sprites = pygame.sprite.Group()
+all_sprites.add(sprite1, sprite2)
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == CHANGE_COLOR:
+            sprite1.change_color()
+            sprite2.change_color()
+    screen.fill((30, 30, 30))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
