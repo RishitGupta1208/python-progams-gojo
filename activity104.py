3871c6d0d94d5560a789ab7e2f5dcd4d1b468053
rishitgupta1208 <rishitgupta1208@gmail.com>
2026-06-12 17:46:50 +0530

    jdj

--- activity104.py
+++ activity104.py
@@ -2,13 +2,11 @@ from tkinter import *
 window=Tk()
 window.title("Event Handler")
 window.geometry('100x100')
-def handle_keypress(event):
-    """Print the character associated to the key pressed"""
-    print(event.char)
-window.bind("<Key>",handle_keypress)
-def handle_click(event):
-    print("\nThe button was clicked")
-button=Button(text="click me")
+countv=0
+def count():
+    global countv
+    countv+=1
+    print("\n",countv)
+button=Button(text="click me",command=count)
 button.pack()
-button.bind("<Button-1>",handle_click)
 window.mainloop()
